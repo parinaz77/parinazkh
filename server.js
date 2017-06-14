ba9b852db33e41cff27362f0074746e70deb2f02
parinaz77 <parinaz77@gmail.com>
2017-06-14 13:16:25 -0700

    add express and server

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
-var http = require('http');
-var fs = require('fs');
+var express = require('express');
+var app = express();
 
-const PORT=8080; 
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
 
-fs.readFile('./index.html', function (err, html) {
+app.get('/', function(req, res) {
+	res.render('index');
+})
 
-    if (err) throw err;    
-
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
-    }).listen(PORT);
-});
+const port = process.env.PORT || 3000
+app.listen(port, process.env.IP, function() {
+	console.log('Listening on port 3000!');
+})
